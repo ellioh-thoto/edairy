9c3d7befcbb23dba3e14713c5a032f5dc7a4368b
ethoto <ethoto@nexway.com>
2016-10-18 21:53:12 +0200

    implement load from ancor + Onhashchange

--- app/scripts/editor.js
+++ app/scripts/editor.js
@@ -7,114 +7,32 @@ var editorTitleNode = document.getElementById('editor-title');
 
 
 (function () {
-	window.onload = function () {
-
-		var editor;
-		ContentTools.StylePalette.add([
-			new ContentTools.Style('Author', 'author', ['p'])
-		]);
-		editor = ContentTools.EditorApp.get();
-		editor.init('*[data-editable]', 'data-name');
-
-		// Handling save notifications
-		editor.addEventListener('saved', function (ev) {
-			var name, payload, regions, xhr;
-
-			// Check that something changed
-			regions = ev.detail().regions;
-			if (Object.keys(regions).length == 0) {
-				return;
-			}
-
-			// Set the editor as busy while we save our changes
-			this.busy(true);
-
-			// Collect the contents of each region into a FormData instance
-			payload = new FormData();
-			var title = document.getElementById("ediary-title").textContent;
-			var filename = document.getElementById("ediary-filename").textContent;
-			payload.append('__title__', title);
-			payload.append('__filename__', filename);
-			console.info('title : ' + title);
-			console.info('filename : ' + filename);
-			for (name in regions) {
-				if (regions.hasOwnProperty(name)) {
-					payload.append(name, regions[name]);
-				}
-			}
-
-			// Send the update content to the server to be saved
-			function onStateChange(ev) {
-				// Check if the request is finished
-				if (ev.target.readyState == 4) {
-					editor.busy(false);
-					if (ev.target.status == '200') {
-						// Save was successful, notify the user with a flash
-						new ContentTools.FlashUI('ok');
-
-					} else {
-						// Save failed, notify the user with a flash
-						new ContentTools.FlashUI('no');
-					}
-				}
-			};
-
-			xhr = new XMLHttpRequest();
-			xhr.addEventListener('readystatechange', onStateChange);
-			xhr.open('POST', 'http://ediary/save.php');
-			xhr.send(payload);
-		});
-
-		// Add support for auto-save
-		editor.addEventListener('start', function (ev) {
-			var _this = this;
-
-			// Call save every 30 seconds
-			function autoSave() {
-				_this.save(true);
-			}
-
-			this.autoSaveTimer = setInterval(autoSave, 5 * 1000);
-		});
-
-		editor.addEventListener('stop', function (ev) {
-			// Stop the autosave
-			clearInterval(this.autoSaveTimer);
-		});
-
-		// init editor
-
-		editorNode.innerHTML = "<p>Le bonheur devrait être déjà là... :/</p>";
-
-		function reqListener(ev) {
-			if (ev.target.readyState === 4) {
-				var content = ev.target.responseText;
-				console.info('Request OK');
-				console.log('ev.target.responseText: '+ ev.target.responseText);
-
-				editorNode.innerHTML = '<p>'+ content +'</p>';
-				titleNode =document.getElementById("ediary-title");
-				if (titleNode != null) {
-					editorTitleNode.innerHTML = document.getElementById("ediary-title").innerHTML;
-					console.info('Editor Title : ' + document.getElementById("ediary-title").innerHTML);
-
-				}
-
-			}
-		}
-		var oReq = new XMLHttpRequest();
-		oReq.onload = reqListener;
-		// oReq.overrideMimeType('application/json');
-		console.info('PARAMS.note : '+ PARAMS.note	);
-		oReq.open("get", "http://ediary/html/" + PARAMS.note + ".html", true);
-		oReq.send();
-	}
+	window.onload = loadPage;
 
 	document.getElementById('newNoteButton').onclick = function () {
 		document.getElementById('newNoteButton').className = "hide";
 		document.getElementById('myform').className = "";
 		document.getElementsByClassName('ct-app').item(0).className = "ct-app hide";
+	};
+
+	window.onhashchange = function () {
+		loadPage();
+	};
+	if (("onhashchange" in window) && !($.browser.msie)) {// event onhashchange supported?
+		window.onhashchange = function () {
+			loadPage();
+		}
 	}
+	else { // event not supported:
+		var prevHash = window.location.hash;
+		window.setInterval(function () {
+			if (window.location.hash != prevHash) {
+				prevHash = window.location.hash;
+				loadPage();
+			}
+		}, 100);
+	}
+
 	}).call(this);
 
 
@@ -155,7 +73,115 @@ function createNote() {
 	'<div id="ediary-title">'+newTitle+'</div>';
 	editorTitleNode.innerHTML=newTitle;
 	document.getElementById('newNoteButton').className = "";
+
 	document.getElementsByClassName('ct-app').item(0).className = "ct-app";
 
-	document.location = "http://ediary/sample.html?note="+newTitle;
+	document.location = "http://ediary/sample.html#note/"+newTitle;
+	document.getElementById("myform").elements['newfile'].value = "";
 }
+
+function loadPage() {
+
+	var editor;
+	ContentTools.StylePalette.add([
+		new ContentTools.Style('Author', 'author', ['p'])
+	]);
+	editor = ContentTools.EditorApp.get();
+	editor.init('*[data-editable]', 'data-name');
+
+	// Handling save notifications
+	editor.addEventListener('saved', function (ev) {
+		var name, payload, regions, xhr;
+
+		// Check that something changed
+		regions = ev.detail().regions;
+		if (Object.keys(regions).length == 0) {
+			return;
+		}
+
+		// Set the editor as busy while we save our changes
+		this.busy(true);
+
+		// Collect the contents of each region into a FormData instance
+		payload = new FormData();
+		var title = document.getElementById("ediary-title").textContent;
+		var filename = document.getElementById("ediary-filename").textContent;
+		payload.append('__title__', title);
+		payload.append('__filename__', filename);
+		console.info('title : ' + title);
+		console.info('filename : ' + filename);
+		for (name in regions) {
+			if (regions.hasOwnProperty(name)) {
+				payload.append(name, regions[name]);
+			}
+		}
+
+		// Send the update content to the server to be saved
+		function onStateChange(ev) {
+			// Check if the request is finished
+			if (ev.target.readyState == 4) {
+				editor.busy(false);
+				if (ev.target.status == '200') {
+					// Save was successful, notify the user with a flash
+					new ContentTools.FlashUI('ok');
+
+				} else {
+					// Save failed, notify the user with a flash
+					new ContentTools.FlashUI('no');
+				}
+			}
+		};
+
+		xhr = new XMLHttpRequest();
+		xhr.addEventListener('readystatechange', onStateChange);
+		xhr.open('POST', 'http://ediary/save.php');
+		xhr.send(payload);
+	});
+
+	// Add support for auto-save
+	editor.addEventListener('start', function (ev) {
+		var _this = this;
+
+		// Call save every 30 seconds
+		function autoSave() {
+			_this.save(true);
+		}
+
+		this.autoSaveTimer = setInterval(autoSave, 5 * 1000);
+	});
+
+	editor.addEventListener('stop', function (ev) {
+		// Stop the autosave
+		clearInterval(this.autoSaveTimer);
+	});
+
+	// init editor
+
+	editorNode.innerHTML = "<p>Le bonheur devrait être déjà là... :/</p>";
+
+	function reqListener(ev) {
+		if (ev.target.readyState === 4) {
+			var content = ev.target.responseText;
+			console.info('Request OK');
+			console.log('ev.target.responseText: ' + ev.target.responseText);
+
+			editorNode.innerHTML = '<p>' + content + '</p>';
+			titleNode = document.getElementById("ediary-title");
+			if (titleNode != null) {
+				editorTitleNode.innerHTML = document.getElementById("ediary-title").innerHTML;
+				console.info('Editor Title : ' + document.getElementById("ediary-title").innerHTML);
+
+			}
+
+		}
+	}
+
+	var oReq = new XMLHttpRequest();
+	oReq.onload = reqListener;
+	// oReq.overrideMimeType('application/json');
+	var note = window.location.hash.substring(1).split('/')[1];
+	// console.info('PARAMS.note : '+ PARAMS.note	);
+	console.info('note : ' + note);
+	oReq.open("get", "http://ediary/html/" + note + ".html", true);
+	oReq.send();
+};
